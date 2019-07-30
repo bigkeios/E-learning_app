670e1eca987aa5be4036e74ce151b9f2ec378cbd
bigkeios <cqphuong97@gmail.com>
2019-07-30 15:15:56 +0700

    Fix routes to static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,6 +8,6 @@ class StaticPagesController < ApplicationController
   def help
   end
 
-  def new
+  def news
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  get '/home', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
+  get '/news', to: 'static_pages#news'
   get '/signup', to: 'users#new'
   resources :users
 end
