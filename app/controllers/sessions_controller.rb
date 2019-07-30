41b0dfcb97c0aa8900d15dfeffecd204564bd5d3
bigkeios <cqphuong97@gmail.com>
2019-07-30 17:42:42 +0700

    Add login and logout with session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  include SessionsHelper
+  # GET /login
+  def new
+  end
+  # POST /login
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to root_path 
+    else
+      flash[:danger] = 'Invalid email or password'
+      render :new
+    end
+  end
+  # DELETE /logout
+  def destroy
+    log_out if logged_in?
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get '/home', to: 'static_pages#home'
@@ -7,5 +8,8 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/news', to: 'static_pages#news'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
