dd6c585e8b4fda14bd287a9dd5e6618716dd0287
bigkeios <cqphuong97@gmail.com>
2019-07-31 14:26:16 +0700

    Fix login errors persisting more than needed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       log_in(user)
       redirect_to root_path 
     else
-      flash[:danger] = 'Invalid email or password'
+      flash.now[:danger] = 'Invalid email or password'
       render :new
     end
   end
