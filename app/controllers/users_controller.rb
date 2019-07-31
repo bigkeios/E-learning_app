3de629b2af3ac428b8f2560ee54664db3a4954bf
bigkeios <cqphuong97@gmail.com>
2019-07-31 17:58:41 +0700

    Add editing user's information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+    include SessionsHelper
+    before_action :logged_in_user?, only: [:show, :index, :edit, :update]
+
     # GET users/new
     def new
         @user = User.new
@@ -20,9 +23,31 @@ class UsersController < ApplicationController
     def index
         @users = User.paginate(page: params[:page], per_page: 20)
     end
+    # GET users/1/edit
+    def edit
+        @user = User.find_by(id: params[:id])
+    end
 
+    #POST users/1/edit
+    def update
+        @user = User.find_by(id: params[:id])
+        if @user.update_attributes(user_params)
+            flash[:success] = "Updated information"
+            redirect_to user_path(@user)
+        else
+            render :edit
+        end
+    end
+    
     private
         def user_params
             params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        end
+
+        def logged_in_user?
+            if !logged_in?
+                flash[:danger] = "Please log in first"
+                redirect_to login_path
+            end
         end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :name, presence: true, length: {maximum:255}
     validates :email, presence: true, length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX}, uniqueness: true
-    validates :password, length: {minimum:5}
-    validates :password_confirmation, presence: true
+    validates :password, length: {minimum:5}, allow_nil: true #allow_nil is for edit info
+    # no need to validate presence of password confirmation bc if it is nil but password is not, they still do not match
     has_secure_password #this will validate presence of password
 
     private
