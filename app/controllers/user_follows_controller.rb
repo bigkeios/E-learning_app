40556285da7468ebe5f6a45fb5c281a3d5ef13dd
bigkeios <cqphuong97@gmail.com>
2019-07-31 19:13:50 +0700

    Add follow/unfollow other users

---
+++ app/controllers/user_follows_controller.rb
@@ -0,0 +1,15 @@
+class UserFollowsController < ApplicationController
+    include SessionsHelper
+    # POST user_follows/
+    def create
+        user = User.find_by(id: params[:followed_id])
+        current_user.follow(user)
+        redirect_to user_path(user)
+    end
+    # DELETE user_follows/1/
+    def destroy
+        user = UserFollow.find_by(id: params[:id]).followed
+        current_user.unfollow(user)
+        redirect_to user_path(user)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,27 @@
 class User < ApplicationRecord
+    has_many :followings, class_name: "UserFollow", foreign_key:"follower_id", dependent: :destroy
+    has_many :followed_users, through: :followings, source: :followed
+    has_many :followeds, class_name: "UserFollow", foreign_key:"followed_id", dependent: :destroy
+    has_many :following_users, through: :followeds, source: :follower
+
     before_save :downcase_email #treat email as unique but ignore case
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :name, presence: true, length: {maximum:255}
     validates :email, presence: true, length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX}, uniqueness: true
     validates :password, length: {minimum:5}, allow_nil: true #allow_nil is for edit info
-    # no need to validate presence of password confirmation bc if it is nil but password is not, they still do not match
     has_secure_password #this will validate presence of password
+
+    def follow(other_user)
+        self.followings.create(followed_id: other_user.id)
+    end
+
+    def unfollow(other_user)
+        self.followings.find_by(followed_id: other_user.id).destroy
+    end
+
+    def following?(other_user)
+        self.followed_users.include?(other_user)
+    end
 
     private
     def downcase_email
