6dd175cd3ecb6a636678d4dc4c02039ac3a2fc94
bigkeios <cqphuong97@gmail.com>
2019-08-01 14:43:52 +0700

    Add CoursesCtr, creating courses

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,25 @@
+class CoursesController < ApplicationController
+  # GET /users
+  def index
+  end
+
+  # GET /users/new
+  def new
+    @course = Course.new
+  end
+
+  # POST /users
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to courses_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def course_params
+      params.require(:course).permit(:name, :desc)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get '/home', to: 'static_pages#home'
@@ -8,4 +7,5 @@ Rails.application.routes.draw do
   get '/news', to: 'static_pages#news'
   get '/signup', to: 'users#new'
   resources :users
+  resources :courses
 end
