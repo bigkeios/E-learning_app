b10ff59a12293a62b6a7cb5bbfa50b3da358e56b
bigkeios <cqphuong97@gmail.com>
2019-08-07 17:44:07 +0700

    Generate course +  add show and index

--- db/schema.rb
+++ db/schema.rb
@@ -10,124 +10,125 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_730_035_245) do
-  create_table 'answers', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
-    t.string 'answer'
-    t.text 'note'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+ActiveRecord::Schema.define(version: 2019_07_30_035245) do
+
+  create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "answer"
+    t.text "note"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'categories', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
-    t.string 'categ'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "categ"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'course_enrolls', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'course_id', null: false
-    t.index ['course_id'], name: 'index_course_enrolls_on_course_id'
-    t.index ['user_id'], name: 'index_course_enrolls_on_user_id'
+  create_table "course_enrolls", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "course_id", null: false
+    t.index ["course_id"], name: "index_course_enrolls_on_course_id"
+    t.index ["user_id"], name: "index_course_enrolls_on_user_id"
   end
 
-  create_table 'courses', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
-    t.string 'name'
-    t.text 'desc'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "courses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.text "desc"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'lesson_results', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
-    t.integer 'correct_ans'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'lesson_id', null: false
-    t.bigint 'user_id', null: false
-    t.index ['lesson_id'], name: 'index_lesson_results_on_lesson_id'
-    t.index ['user_id'], name: 'index_lesson_results_on_user_id'
+  create_table "lesson_results", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "correct_ans"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "lesson_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["lesson_id"], name: "index_lesson_results_on_lesson_id"
+    t.index ["user_id"], name: "index_lesson_results_on_user_id"
   end
 
-  create_table 'lessons', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
-    t.string 'name'
-    t.string 'objective'
-    t.bigint 'course_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['course_id'], name: 'index_lessons_on_course_id'
+  create_table "lessons", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "objective"
+    t.bigint "course_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_id"], name: "index_lessons_on_course_id"
   end
 
-  create_table 'question_answers', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.bigint 'question_id', null: false
-    t.boolean 'correct'
-    t.string 'answer_user'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['question_id'], name: 'index_question_answers_on_question_id'
-    t.index ['user_id'], name: 'index_question_answers_on_user_id'
+  create_table "question_answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "question_id", null: false
+    t.boolean "correct"
+    t.string "answer_user"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_question_answers_on_question_id"
+    t.index ["user_id"], name: "index_question_answers_on_user_id"
   end
 
-  create_table 'questions', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
-    t.text 'question'
-    t.bigint 'word_id', null: false
-    t.bigint 'answer_id', null: false
-    t.bigint 'lesson_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['answer_id'], name: 'index_questions_on_answer_id'
-    t.index ['lesson_id'], name: 'index_questions_on_lesson_id'
-    t.index ['word_id'], name: 'index_questions_on_word_id'
+  create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.text "question"
+    t.bigint "word_id", null: false
+    t.bigint "answer_id", null: false
+    t.bigint "lesson_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["answer_id"], name: "index_questions_on_answer_id"
+    t.index ["lesson_id"], name: "index_questions_on_lesson_id"
+    t.index ["word_id"], name: "index_questions_on_word_id"
   end
 
-  create_table 'user_follows', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
-    t.integer 'follower_id'
-    t.integer 'followed_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "user_follows", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.string 'password_digest'
-    t.string 'avatar'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.boolean 'admin'
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "avatar"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
   end
 
-  create_table 'word_learnts', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
-    t.bigint 'word_id', null: false
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['user_id'], name: 'index_word_learnts_on_user_id'
-    t.index ['word_id'], name: 'index_word_learnts_on_word_id'
+  create_table "word_learnts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "word_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_word_learnts_on_user_id"
+    t.index ["word_id"], name: "index_word_learnts_on_word_id"
   end
 
-  create_table 'words', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4', force: :cascade do |t|
-    t.string 'word'
-    t.text 'definition'
-    t.bigint 'category_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['category_id'], name: 'index_words_on_category_id'
+  create_table "words", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "word"
+    t.text "definition"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_words_on_category_id"
   end
 
-  add_foreign_key 'course_enrolls', 'courses'
-  add_foreign_key 'course_enrolls', 'users'
-  add_foreign_key 'lesson_results', 'lessons'
-  add_foreign_key 'lesson_results', 'users'
-  add_foreign_key 'lessons', 'courses'
-  add_foreign_key 'question_answers', 'questions'
-  add_foreign_key 'question_answers', 'users'
-  add_foreign_key 'questions', 'answers'
-  add_foreign_key 'questions', 'lessons'
-  add_foreign_key 'questions', 'words'
-  add_foreign_key 'word_learnts', 'users'
-  add_foreign_key 'word_learnts', 'words'
-  add_foreign_key 'words', 'categories'
+  add_foreign_key "course_enrolls", "courses"
+  add_foreign_key "course_enrolls", "users"
+  add_foreign_key "lesson_results", "lessons"
+  add_foreign_key "lesson_results", "users"
+  add_foreign_key "lessons", "courses"
+  add_foreign_key "question_answers", "questions"
+  add_foreign_key "question_answers", "users"
+  add_foreign_key "questions", "answers"
+  add_foreign_key "questions", "lessons"
+  add_foreign_key "questions", "words"
+  add_foreign_key "word_learnts", "users"
+  add_foreign_key "word_learnts", "words"
+  add_foreign_key "words", "categories"
 end
