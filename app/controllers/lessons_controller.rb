ea5c2e739e277da92de8a42702e9cbc65769c237
bigkeios <cqphuong97@gmail.com>
2019-08-08 16:04:40 +0700

    Add empty lessons list, new/edit lessons

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,35 @@
+class LessonsController < ApplicationController
+  include SessionsHelper
+  before_action :logged_in_user, only: %i[new create]
+  before_action :admin_user, only: %i[new create]
+  before_action :set_course, only: %i[new create]
+
+  # GET courses/1/lessons/new
+  def new
+    @lesson = Lesson.new
+  end
+
+  # POST courses/1/lessons
+  def create
+    @lesson = @course.lessons.build(lesson_params)
+    if @lesson.save
+      flash[:success] = t :create_lesson_succ
+      redirect_to course_path()
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def lesson_params
+    params.require(:lesson).permit(:name, :objective)
+  end
+
+  def set_course
+    @course = Course.find_by(id: params[:course_id])
+    return if @course
+    flash[:danger] = t :no_course
+    redirect_to root_path
+  end
+end
--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,5 @@ class Course < ApplicationRecord
   validates :name, presence: true, length: { minimum: 10, maximum: 255 }
   validates :desc, presence: true, length: { minimum: 50 }
   default_scope { where(deleted: false) }
+  has_many :lessons
 end
