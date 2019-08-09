cd25de7dabdcbb5c01e70ec7f1e1a826c7e26f6d
bigkeios <cqphuong97@gmail.com>
2019-08-09 13:37:33 +0700

    Revert "Add import categs from csv file"
    This reverts commit 36b85eca63ec4d64ac98f3175300827b9e05ec86.

--- app/controllers/categories_controller.rb
+++
@@ -1,9 +0,0 @@
-class CategoriesController < ApplicationController
-  def import
-    if Category.import(params[:file])
-        flash[:success] = t :categ_imported_succ
-    else
-      flash[:danger] = t :categ_imported_fail
-    end
-  end
-end
--- test/controllers/categories_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CategoriesControllerTest < ActionDispatch::IntegrationTest
-  test "should get import" do
-    get categories_import_url
-    assert_response :success
-  end
-
-end
